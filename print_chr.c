2767b9d4af401db505d0a74939042ac70ef80a5a
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-28 13:11:12 +0200

    Update and rename main_function.c to print_chr.c

---
+++ print_chr.c
@@ -0,0 +1,20 @@
+nes (16 sloc)  353 Bytes
+
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chr - writes the character c to stdout
+ * @arguments: input char
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: On success 1.
+ */
+int print_chr(va_list args, char *buffer, unsigned int nbytes)
+{
+	char c;
+
+	c = va_arg(args, int);
+	handle_buffer(buffer, c, nbytes);
+
+	return (1);
+}
