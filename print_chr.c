0c136cc04d6d65a4b86f3113872c194eceb49c77
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-28 13:11:23 +0200

    Update print_chr.c

--- print_chr.c
+++ print_chr.c
@@ -2,13 +2,7 @@ nes (16 sloc)  353 Bytes
 
 #include "main.h"
 #include <stdio.h>
-/**
- * print_chr - writes the character c to stdout
- * @arguments: input char
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: On success 1.
- */
+
 int print_chr(va_list args, char *buffer, unsigned int nbytes)
 {
 	char c;
