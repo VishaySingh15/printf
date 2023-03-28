c40226b0fa1c2fb8148b3e22284491a5a3b80b94
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-28 09:29:19 +0200

    free ch malloc

--- print_format.c
+++ print_format.c
@@ -12,23 +12,24 @@ int print_format(va_list args, const char *format, int i)
 {
 	int count_str;
 	char *str;
-	char *c = malloc(sizeof(char) * 2);
+	char *ch = malloc(sizeof(char) * 2);
 
 	if (format[i + 1] == 's')
 	{
 		str = va_arg(args, char *);
 		count_str = get_count(str);
+		free(ch);
 		return(write(1, str, count_str));
 	}
 	else if (format[i + 1] == 'c')
 	{
-		c[0] = va_arg(args, int);
-		return(write(1, c, 1));
+		ch[0] = va_arg(args, int);
+		return(write(1, ch, 1));
 	}
 	else if (format[i + 1] == '%')
 	{
-		c[0] = '%';
-		return(write(1, c, 1));
+		ch[0] = '%';
+		return(write(1, ch, 1));
 	}
 	else
 	{
