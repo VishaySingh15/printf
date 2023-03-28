670e88070e55c659eecac721e8506b01d09c03c3
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-28 13:09:18 +0200

    Update main.h

--- main.h
+++ main.h
@@ -2,10 +2,21 @@
 #define _MAIN_
 
 #include <stdarg.h>
+#include <unistd.h>
 #include <stdlib.h>
 
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
 int _printf(const char *format, ...);
-int get_count(const char *format);
-int print_format(va_list args, const char *format, int i);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+int print_str(va_list arguments, char *buf, unsigned int ibuf);
+
+int (*get_print_function(const char *s, int index))(va_list, char *, unsigned int);
+unsigned int handle_buffer(char *buf, char c, unsigned int ibuf);
+int print_buffer(char *buf, unsigned int nbuf);
 
 #endif
