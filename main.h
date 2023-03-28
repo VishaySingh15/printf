ec1cf4c8867b59545a7c557f4ce442d8e5085d9e
Vishay Singh <113976618+VishaySingh15@users.noreply.github.com>
2023-03-28 09:03:19 +0200

    created main.h with prototypes

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int get_count(const char *format);
+int print_format(va_list args, const char *format, int i);
+
+#endif
